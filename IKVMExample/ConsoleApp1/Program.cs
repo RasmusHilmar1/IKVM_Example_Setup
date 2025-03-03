a22eb11e5449b50869fb622e3e0760d7342fac21
Rasmus Hilmar <rasmus.hilmar@visma.com>
2025-03-03 11:53:40 +0100

    4.8 Maven usaged

---
+++ IKVMExample/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using IKVMExample;
+
+
+namespace IKVMConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call into your IKVMExample library
+            try
+            {
+                Console.WriteLine("Starting PDF generation...");
+                PdfGenerator.GeneratePdf();
+                Console.WriteLine("PDF generation completed!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ IKVMExample/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Visma Group")]
+[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyCopyright("Copyright © Visma Group 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0a9b8e8f-5459-4b40-9892-4dfc72d9fd11")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
